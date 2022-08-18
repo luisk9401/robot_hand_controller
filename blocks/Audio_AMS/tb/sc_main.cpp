bc749b028fb724b1109d476aad547f79b5876ce6
Kenneth <kecordoba@estudiantec.cr>
2022-08-17 23:46:34 -0600

    Agregado lector de archivos Wav al TB

--- blocks/Audio_AMS/tb/sc_main.cpp
+++ blocks/Audio_AMS/tb/sc_main.cpp
@@ -1,6 +1,7 @@
 #include "systemc.h"
 #include "systemc-ams.h"
 #include "sin_src.cpp"
+#include "wav_src.cpp"
 
 //#include "LPF.cpp"
 //#include "ADC.cpp"
@@ -17,23 +18,20 @@ int sc_main(int argc, char* argv[])
  	
  	const sc_time t_stop(1.50, SC_MS);
  	// ADC sampling period (equal to TDF time step)
- 	const sc_time dt_adc(5.0, SC_NS);
+ 	const sc_time dt_adc(5.0, SC_NS);	
 
- 	
-  //sc_core::sc_set_time_resolution(10.0, sc_core::SC_NS);
-
-  //sca_util::sca_information_off();
-  // Resolution of the ADC
+  	// Resolution of the ADC
  	const int NBitsADC = 16;
   
   	sca_tdf::sca_signal<double> wave_in("wave_in");
   
-  	sca_tdf::sca_signal<sc_dt::sc_int<NBitsADC> > adc_sig("adc_sig");
+  	sca_tdf::sca_signal<sc_dt::sc_int<NBitsADC> > adc_sig("adc_sig"); 	
 
- 	
-
-  	sin_src sine("sine", 1.0 , 1.0e3, sca_core::sca_time( 5.0, sc_core::SC_NS ) );
-  	sine.out(wave_in);
+  	//sin_src sine("sine", 1.0 , 1.0e3, sca_core::sca_time( 5.0, sc_core::SC_NS ) );
+  	//sine.out(wave_in);
+  	
+  	wav_src wav("wav", 1.0 , sca_core::sca_time( 5.0, sc_core::SC_NS ) );
+  	wav.out(wave_in);
 
   	Audio_receiver<NBitsADC> receiver("receiver",20000.,dt_adc, v_supply);
   	receiver.wave_in(wave_in);
